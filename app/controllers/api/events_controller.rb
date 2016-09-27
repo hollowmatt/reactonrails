22a5321a18987ee24356635dd3902c07330f5d20
Matt Holloway <mholloway@book.com>
2016-09-27 14:18:58 -0400

    added create funcxtionality

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -12,6 +12,19 @@ module Api
       render json: events
     end
 
-  end
+    def create
+      event = Event.new(event_params)
+      if event.save
+        render json: event
+      else
+        render nothing: true, status: :bad_request
+      end
+    end
 
+    private
+
+      def event_params
+        params.require(:event).permit(:name, :description, :event_date, :place)
+      end
+  end
 end
