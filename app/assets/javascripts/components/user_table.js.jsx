07335b9d23bd8222eca1545b94f48fa34cad3b80
Matt Holloway <mholloway@book.com>
2016-11-01 08:32:17 -0400

    Add a key

--- app/assets/javascripts/components/user_table.js.jsx
+++ app/assets/javascripts/components/user_table.js.jsx
@@ -3,7 +3,7 @@ var UserTable = React.createClass({
   render: function() {
     var users = [];
     this.props.users.forEach(function(user) {
-      users.push(<User user={user}/>);
+      users.push(<User user={user} key={'user' +user.id}/>);
     }.bind(this));
     return(
       <table className="table table-striped">
